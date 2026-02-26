a1d0f4164466f7046433530bb73928d99c5eb3cd
Jakub <01199229@pw.edu.pl>
2026-02-26 21:59:21 +0100

    dodanie XGBoosta wraz z HPO oraz dodanie HPO do random forest

--- Nootebooks/dataProcessing.py
+++ Nootebooks/dataProcessing.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso, Ridge, ElasticNet
 from sklearn.pipeline import Pipeline
@@ -7,7 +7,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import mean_squared_error, r2_score
-
+import category_encoders as ce
 
 
 def processData(data):
@@ -22,7 +22,7 @@ def processData(data):
     data[columnName] = data[columnName].map({'Not_Canceled': 0, 'Canceled': 1})
 
     X_train, X_test, y_train, y_test, num_features, cat_features = splitData(data,columnName)
-    X_train_processed, X_test_processed = setTrainngDataSet(X_train, X_test, num_features, cat_features)
+    X_train_processed, X_test_processed = setTrainngDataSet(X_train, X_test, y_train, num_features, cat_features)
 
     return X_train_processed, X_test_processed, y_train, y_test
 
@@ -50,24 +50,22 @@ def splitData(data, columnName):
     return X_train, X_test, y_train, y_test, num_features, cat_features
 
 
-def setTrainngDataSet(X_train, X_test, num_features, cat_features):
+def setTrainngDataSet(X_train, X_test, y_train, num_features, cat_features):
     
-    # Pipeline dla cech numerycznych
-
+    # Pipeline dla cech numerycznych (Bez zmian)
     num_pipeline = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median')),
         ('scaler', StandardScaler())
     ])
 
     # Pipeline dla cech kategorycznych
-
     cat_pipeline = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='most_frequent')),
-        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+        # ### ZMIANA 4: Podmieniamy OneHotEncoder na TargetEncoder
+        ('target_encoder', ce.TargetEncoder()) 
     ])
 
-    # Łączenie pipeline'ów
-
+    # Łączenie pipeline'ów (Bez zmian)
     preprocessor = ColumnTransformer(transformers=[
         ('num', num_pipeline, num_features),
         ('cat', cat_pipeline, cat_features)
@@ -77,9 +75,8 @@ def setTrainngDataSet(X_train, X_test, num_features, cat_features):
         ('preprocessor', preprocessor)
     ])
 
-    # Dostawienie przetworzonych danych do danych treningowych
-
-    data_pipeline.fit(X_train)
+    # ### ZMIANA 5: Target Encoder MUSI widzieć y_train, żeby policzyć prawdopodobieństwa!
+    data_pipeline.fit(X_train, y_train)
 
     features_names = (
         pd.Index(data_pipeline.named_steps['preprocessor'].get_feature_names_out())
@@ -93,37 +90,37 @@ def setTrainngDataSet(X_train, X_test, num_features, cat_features):
     print("Processed training dataset: ") 
     print(X_train_processed.head())
 
-    return X_train_processed, X_test_processed,
+    return X_train_processed, X_test_processed
 
-import pandas as pd
-import numpy as np
+
+
 
 def create_features(df):
     print("Rozpoczynam Inżynierię Cech...")
     data = df.copy()
-    date_str = data['arrival_year'].astype(str) + '-' + \
-               data['arrival_month'].astype(str) + '-' + \
-               data['arrival_date'].astype(str)
+    #date_str = data['arrival_year'].astype(str) + '-' + \
+    #           data['arrival_month'].astype(str) + '-' + \
+    #           data['arrival_date'].astype(str)
                
-    full_date = pd.to_datetime(date_str, errors='coerce')
-    data['arrival_day_of_week'] = full_date.dt.dayofweek
-    data['arrival_day_of_week'] = data['arrival_day_of_week'].fillna(-1).astype(int)
-    data['is_arrival_weekend'] = (data['arrival_day_of_week'] >= 5).astype(int)
-    data['total_guests'] = data['no_of_adults'] + data['no_of_children']
-    data['total_nights'] = data['no_of_weekend_nights'] + data['no_of_week_nights']
-    data['price_per_person'] = data['avg_price_per_room'] / data['total_guests'].replace(0, 1)
+    #full_date = pd.to_datetime(date_str, errors='coerce')
+    #data['arrival_day_of_week'] = full_date.dt.dayofweek
+    #data['arrival_day_of_week'] = data['arrival_day_of_week'].fillna(-1).astype(int)
+    #data['is_arrival_weekend'] = (data['arrival_day_of_week'] >= 5).astype(int)
+    #data['total_guests'] = data['no_of_adults'] + data['no_of_children']
+    #data['total_nights'] = data['no_of_weekend_nights'] + data['no_of_week_nights']
+    data['price_per_person'] = data['avg_price_per_room'] / (data['no_of_adults'] + data['no_of_children']).replace(0, 1)
     # Przedziały: 0-7 dni (Last minute), 8-30 dni (Krótki), 31-90 (Średni), 91-180 (Długi), 180+ (Bardzo długi)
-    bins = [-1, 7, 30, 90, 180, 10000]
-    labels = [0, 1, 2, 3, 4]
-    data['lead_time_category'] = pd.cut(data['lead_time'], bins=bins, labels=labels).astype(int)
-    kolumny_do_usuniecia = [
-        'arrival_year', 
-        'arrival_month', 
-        'arrival_date', 
-        'avg_price_per_room',
-        'lead_time_category'
-    ]
+    #bins = [-1, 7, 30, 90, 180, 10000]
+    #labels = [0, 1, 2, 3, 4]
+    #data['lead_time_category'] = pd.cut(data['lead_time'], bins=bins, labels=labels).astype(int)
+    #kolumny_do_usuniecia = [
+    #    'arrival_year', 
+    #    'arrival_month', 
+    #    'arrival_date', 
+    #    'avg_price_per_room',
+    #    'lead_time_category'
+    #]
 
-    data = data.drop(columns=kolumny_do_usuniecia)
+    #data = data.drop(columns=kolumny_do_usuniecia)
     return data
 
--- Nootebooks/randomForest.py
+++ Nootebooks/randomForest.py
@@ -1,23 +1,39 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-from matplotlib.colors import ListedColormap
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 def randomForestTraining(X_train, y_train):
+    print("Rozpoczynam Optymalizację Hiperparametrów dla Random Forest...")
+    
+    param_grid = {
+        'n_estimators': [100, 200, 300, 400, 500],
+        'max_depth': [10, 15, 20, 25, 30, None],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4],
+        'max_features': ['sqrt', 'log2'],
+        'class_weight': [None, 'balanced', 'balanced_subsample']
+    }
 
-    print("inicializing RandomForestClassifier")
-    rf = RandomForestClassifier()
-
-    print("Training...")
-    rf.fit(X_train,y_train)
+    rf_base = RandomForestClassifier(random_state=42, n_jobs=-1)
 
 
-    return rf
+    random_search = RandomizedSearchCV(
+        estimator=rf_base,
+        param_distributions=param_grid,
+        n_iter=20,
+        scoring='f1',
+        cv=5,
+        verbose=2,
+        random_state=42,
+        n_jobs=-1
+    )
+
+    random_search.fit(X_train, y_train)
+
+    print("\nOptymalizacja zakończona")
+    print(f"Najlepsze parametry: {random_search.best_params_}")
+    print(f"Najlepszy średni F1-Score z CV: {random_search.best_score_:.4f}")
+
+    return random_search.best_estimator_
+
 
 
